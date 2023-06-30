13aabea31059da990828f3f6b8b6919c95838a76
donfortune <donfortunet.df@gmail.com>
2023-06-30 10:04:49 +0100

    linked open ai api

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import typing
 from PyQt6 import QtCore
 from  PyQt6.QtWidgets import QMainWindow, QWidget, QTextEdit, QLineEdit, QPushButton, QApplication
 import sys
+import openai 
 
 
 class ChatbotWindow(QMainWindow):
@@ -27,8 +28,24 @@ class ChatbotWindow(QMainWindow):
     
 
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = "xxxxxxxxxxxx"
 
+    def receive_response(self, user_input):
+        response = openai.Completion.create(
+            engine = 'text-davinci-003',   #open ai model
+            prompt = user_input,
+            max_tokens = 4000,  #returns longer answers
+            temperature = 0.5 #accuracy of answers
+
+        ).choices[0].message.content
+        return response
+
+        
+if __name__ == "__main__":
+    Chatbot = Chatbot()
+    response = Chatbot.receive_response('tell me a joke')
+    print(response)
 
 app = QApplication(sys.argv)
 main_window = ChatbotWindow()
