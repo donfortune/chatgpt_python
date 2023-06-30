d1bbc03f6684f7a23b9f488532d566de60e08ee0
donfortune <donfortunet.df@gmail.com>
2023-06-30 11:37:01 +0100

    configure the chatbot implementation with the api

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import openai
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.chatbot = Chatbot()
         self.setMinimumSize(700, 500)
 
         self.chat_box = QTextEdit(self)
@@ -23,29 +23,37 @@ class ChatbotWindow(QMainWindow):
 
         self.button = QPushButton('Submit', self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.submit_request)
 
         self.show()
+
+    def submit_request(self):
+        user_input = self.input_box.text().strip()
+        self.chat_box.append(f"Prompt: {user_input}")
+        self.input_box.clear()
+
+        response = self.chatbot.receive_response(user_input)
+        self.chat_box.append(f"Bot: {response}")
+
+
+
     
 
 class Chatbot:
     def __init__(self):
-        openai.api_key = "xxxxxxxxxxxx"
+        openai.api_key = "xxxxxxxxxxxxxx"
 
     def receive_response(self, user_input):
         response = openai.Completion.create(
-            engine = 'text-davinci-003',   #open ai model
-            prompt = user_input,
-            max_tokens = 4000,  #returns longer answers
-            temperature = 0.5 #accuracy of answers
+            engine='text-davinci-003',  
+            prompt=user_input,
+            max_tokens=4000,  
+            temperature=0.5 
 
         ).choices[0].message.content
         return response
 
         
-if __name__ == "__main__":
-    Chatbot = Chatbot()
-    response = Chatbot.receive_response('tell me a joke')
-    print(response)
 
 app = QApplication(sys.argv)
 main_window = ChatbotWindow()
